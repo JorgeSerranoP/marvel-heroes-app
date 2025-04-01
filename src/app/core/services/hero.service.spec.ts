10962303b4de936300af0d3fed576b785c9b2c5f
Jorge <jorgeserranoperez00@gmail.com>
2025-04-01 18:57:41 +0200

    fix imports

--- src/app/core/services/hero.service.spec.ts
+++ src/app/core/services/hero.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { HeroService } from './hero.service';
-import { Hero } from '../models/hero';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Hero } from '../models/hero';
+import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
   let service: HeroService;
